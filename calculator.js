b06d1f65c11f2ffe4d70e05dd3a85a0f66360748
mohalenezi <ot.m.alenezi@gmail.com>
2021-05-24 19:35:02 +0300

    Solved the Conditionals task

--- calculator.js
+++ calculator.js
@@ -1,6 +1,11 @@
-const prompt = require("prompt-sync")(); // Please don't touch me :)
 
 // The prompt method allows us to take an input from the user.
 // Run the code to see how to use prompt.
-const name = prompt("What's your name? ");
-console.log(`Hi, I'm ${name}.`);
+
+let number1 = 3
+let number2 = 9
+let operation = "-"
+if (operation === "+") console.log (`${number1} + ${number2}=${number1+number2}`);
+else if (operation === "/") console.log (`${number1} / ${number2}=${number1/number2}`);
+else if (operation === "*") console.log (`${number1} * ${number2}=${number1*number2}`)
+else if (operation === "-") console.log (`${number1} - ${number2}=${number1-number2}`)
